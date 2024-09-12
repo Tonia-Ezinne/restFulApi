90906a6d5955dcf52d77cb1a41b12c7122d75c0a
Tonia-Ezinne <toniazinny13@gmail.com>
2024-09-12 19:52:52 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors"); // Import CORS
+const connectDB = require("../config/db");
+const resourceRoutes = require("../routes/resourceRoutes");
+const errorHandler = require("../middleware/errorHandler");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// CORS configuration
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || "http://localhost:3000", // Your frontend URL
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+// Enable CORS with the specified options
+app.use(cors(corsOptions));
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Define routes
+app.use("/api", resourceRoutes);
+
+// Error Handling Middleware
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
